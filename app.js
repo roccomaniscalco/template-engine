4bd15bf6b06be2cc5fac1259e734e5e90debbe15
Rocco <roccomaniscalco2001@gmail.com>
2020-11-10 22:06:30 -0500

    created function to  write html filein /output

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-const Choices = require("inquirer/lib/objects/choices");
-const Employee = require("./lib/Employee");
 
 let teamName;
 let employeeType;
 let employeeCount = 0;
 const employees = [];
+
+generateHTML = () => {
+  const content = render(employees);
+  fs.writeFile("./output/team.html", content, (err) => {
+    if (err) throw err;
+    console.log();
+    console.log("team.html successfully created in /output!");
+  });
+};
 
 promptTeamName = () => {
   inquirer
@@ -32,7 +34,7 @@ promptTeamName = () => {
 };
 
 promptEmployeeType = () => {
-  console.log("");
+  console.log();
   inquirer
     .prompt([
       {
@@ -44,7 +46,8 @@ promptEmployeeType = () => {
     ])
     .then((employee) => {
       employeeType = employee.type;
-      if (employeeType != "Exit") promptEmployeeInfo();
+      if (employeeType == "Exit") generateHTML();
+      else promptEmployeeInfo();
     });
 };
 
@@ -132,23 +135,3 @@ promptEmployeeInfo = () => {
 };
 
 promptTeamName();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
